ac85ce11474f7568d67d574dc2d35cbedda1f8ed
Gabriel Pereira Soares <gabriel.soares.forcode@gmail.com>
2023-05-10 10:54:31 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Hellow Word!</h1>
+       <h1>Hello Word!</h1>
       </header>
     </div>
   );
